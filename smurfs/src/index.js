d0b8a4153ac22ba0cbfb096bc7378f95a893b9b0
Steven <38477361+Steven-matos@users.noreply.github.com>
2020-02-21 13:54:18 -0500

    added provider and created store

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+
+import {smurfsReducer} from './reducers/smurfsReducer';
 import "./index.css";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(smurfsReducer, applyMiddleware(thunk));
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById("root"));
