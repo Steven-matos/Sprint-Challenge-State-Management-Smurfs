c6c86b682fce840470a2128fca479b067ef3026c
Steven <38477361+Steven-matos@users.noreply.github.com>
2020-02-21 11:44:32 -0500

    created reducer for smurfs

---
+++ smurfs/src/reducers/smurfsReducer.js
@@ -0,0 +1,13 @@
+
+const initialState = {
+    smurfs: [],
+    isFetchingData: false,
+    error: ''
+}
+
+export const smurfsReducer = (state = initialState, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+};
